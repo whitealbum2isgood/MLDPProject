af956d00ce24080f337d523e3243a4dee63de154
whitealbum2isgood <2405137G@student.tp.edu.sg>
2025-07-30 18:18:11 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ all_categories = ['ART', 'AUTOMOTIVE', 'BUSINESS', 'COMMUNICATION', 'EDUCATION',
                   'ENTERTAINMENT', 'HEALTH', 'LIFESTYLE', 'SHOPPING',
                   'TRAVEL', 'SYSTEM']
 
-# Input widgets
 reviews = st.number_input("Number of Reviews", min_value=0)
 price = st.number_input("Price ($)", min_value=0.0)
 size = st.number_input("Size (MB)", min_value=0.0)
@@ -41,7 +40,6 @@ installs = st.number_input("Number of Installs", min_value=0)
 category = st.selectbox("App Category", ['No Category'] + all_categories)
 category_encoded = [1 if category == cat else 0 for cat in all_categories]
 
-# Example input array — expand this to match your actual model input
 input_data = np.array([[reviews, price, size, installs]+ category_encoded])
 
 if st.button("Predict Rating"):
